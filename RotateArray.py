8deac517dcc29ed07b4468f575380e9920701d33
chydrio <75637885+chydrio@users.noreply.github.com>
2020-12-07 13:59:50 -0500

    Create RotateArray.py

---
+++ RotateArray.py
@@ -0,0 +1,8 @@
+def rotate(nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        nums.reverse()
+        nums[:k]=reversed(nums[:k])
+        nums[k:]=reversed(nums[k:])
